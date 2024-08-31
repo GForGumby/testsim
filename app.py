c3687de32340a1c12388b2fe9f51834dd7c7836d
Gumby <156488743+GForGumby@users.noreply.github.com>
2024-08-31 18:50:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -203,10 +203,7 @@ def run_parallel_simulations(num_simulations, draft_results_df, projection_looku
     
     return final_results
 
-# Streamlit app
-sample_csv_path = 'NFL WEEK 1 ADP TEMPLATE.csv'
-with open(sample_csv_path, 'rb') as file:
-    sample_csv = file.read()
+
 
 
 
